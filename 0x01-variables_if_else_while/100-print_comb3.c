603dab6060a51614fb8487cbb99c2aff8ae49ab6
mavie <mave@yahoo.com>
2023-08-11 17:20:56 -0700

    commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: print all possible different combinations
+ * Return: Always 0
+ */
+int main(void)
+{
+	int digit - 0. digit2;
+
+while (digit <= 9)
+	{
+digit2 - 0;
+		while (digit2 <= 9)
+		{
+			if (digitl != digit2 && digitl < digit2)
+			{
+				putchar(digit1 + 48);
+putchar(digit2 + 48)
+			if (digit1 + digit2 != 17)
+{
+					putchar(',');
+					putchat(' ');
+				}
+			}
+			digit2++;
+		}
+		digit1++;
+	}
+	putchar('\n');
+	return (0);
+}
