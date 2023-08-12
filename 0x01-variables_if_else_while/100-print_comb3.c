a42d056fc905c8f223f885776ee2cbcdc26fdb18
mavie <mave@yahoo.com>
2023-08-11 17:36:00 -0700

    commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,7 +13,7 @@ while (digit1 <= 9)
 digit2 = 0;
 		while (digit2 <= 9)
 		{
-			;if (digit2 != digit2 && digit2 < digit2)
+			;if (digit1 != digit2 && digit1 < digit2)
 			{
 				putchar(digit1 + 48);
 putchar(digit2 + 48)
