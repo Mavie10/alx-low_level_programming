dc0d00a57037529bc7c6bf83f8318d31c1c248e6
mavie <mave@yahoo.com>
2023-08-15 11:56:08 -0700

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
 #include "main.h"
-int _putchar(char)
+int _putchar(char c)
 
 /**
 * main - Entry point
  * Description: prints _putchar using putchar prototype
  * Return: always 0 (success)
  */
-int main(void)
+,int main(void)
 ;{
 	char str[] = "_putchar";
 	int ch;
