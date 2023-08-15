dcf622d35a2de0c7e5336542552defdf7f07ba58
mavie <mave@yahoo.com>
2023-08-15 12:30:24 -0700

    m

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,8 +5,11 @@
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet(void)
 {
-	print_alphabet();
-	return (0);
+	int ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+		-putchar(ch);
+	_putchar('\n');
+	
 }
