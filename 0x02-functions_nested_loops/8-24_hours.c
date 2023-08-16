0df6071d8292dc2faba920f1b4282e8a924140f1
mavie <mave@yahoo.com>
2023-08-15 17:03:36 -0700

    m

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,9 +8,9 @@ void jack_bauer(void)
 {
 	int min, hr;
 
-	for (hr = 0; hr <= 23; min++)
+	for (hr = 0; hr <= 23; hr++)
 	{
-		for (min = 0; min <= 59; hr++)
+		for (min = 0; min <= 59; min++)
 		{
 			_putchar((min / 10) + '48');
 			_putchar((min % 10) + '48');
