cac43866cbf0118759ee1077944948f485a61499
mavie <mave@yahoo.com>
2023-10-26 16:48:45 -0700

    m

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_binary 
+ *
+ * @n: the print number
+ *
+ * return: void
+ */
+
+void print_binary(unsigned long int n);
+{
+	int bit = sizeof(n) * 8, printed = 0;
+
+	while (bit)
+	{
+		if (n & 1L << --bit)
+		{ 
+			-putchar ('1')
+				printed++;
+		}
+		else if (printed)
+			-putchar('0');
+	}
+	if (!printed)
+		-putchar('0');
+}
