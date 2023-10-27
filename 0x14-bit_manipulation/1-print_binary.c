37107c5b18e732fdf3d15f6c6e8b7451092b08af
mavie <mave@yahoo.com>
2023-10-26 18:33:49 -0700

    m

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -15,12 +15,13 @@ void print_binary(unsigned long int n)
 	while (bit)
 	{
 		if (n & 1L << --bit)
-{
-			_putchar ('1');
+		{
+			_putchar('1');
 			printed++;
 		}
-	else if (printed)
-			_putchar('0');	
+		else if (printed)
+			_putchar('0');
+	}
 	if (!printed)
 		_putchar('0');
 }
