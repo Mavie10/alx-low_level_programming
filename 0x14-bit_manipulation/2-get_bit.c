aa24e3993d2f4f1d6b99ea41bfb07529be02d8d3
mavie <mave@yahoo.com>
2023-10-26 17:13:57 -0700

    m

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * gets the bit at index
+ *
+ * @n: the number to index
+ * @index: the bit to get
+ *
+ * Return : the bit state 
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= sizeof(n) *8)
+		return(-1);
+	return(n >> index & 1);
+}
