44e3aa73a87d93b5f6dc8773978d10e6849c7e6a
mavie <mave@yahoo.com>
2023-10-26 17:47:17 -0700

    m

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * flio_bits - the numbers that need to be flippes
+ * @n:first number
+ * @m:second number
+ *
+ * return:bits that flip 
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xorval = n ^ m;
+	unsigned int count = 0;
+	
+	while (xorval)
+	{
+		if (xorval & 1ul)
+			count++;
+		xorval = xorval >> 1;
+	}
+	return (count);
+}
