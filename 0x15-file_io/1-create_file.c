baf4503311785e5d4f5de1ebcda8f0d2da03c316
mavie <mave@yahoo.com>
2023-10-30 16:28:53 -0700

    m

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 	for (nletters = 0; text_content[nletters]; nletters++);
 
 	rwr = write(fd, text_content, nletters);
-	if (rwrw == -1)
+	if (rwr == -1)
 		return (-1);
 	close(fd);
 	return (1);
