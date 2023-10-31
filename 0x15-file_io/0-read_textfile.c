f978a6b42bab736bc20e6dcd516a27fbfd859461
mavie <mave@yahoo.com>
2023-10-30 18:29:06 -0700

    m

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,19 +1,18 @@
 #include "main.h"
- 
 
 /**
- *ssize_t read_textfile -  reads a file and prints it to the standard output
+ *read_textfile - Reads a file and prints its content to the standard output
+ * @filename: Name of the file to read
+ * @letters: The number of bytes to read and print
  *
- * @filname:name of the file to read
- * @letters: the number of bytes
- *
- * return: actual number of letters it could read and print
+ * Return: The actual number of bytes read and printed. -1 on failure
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t bytes;
 	char buf[READ_BUF_SIZE * 8];
+
 	if (!filename || !letters)
 		return (-1);
 	fd = open(filename, O_RDONLY);
@@ -23,13 +22,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (bytes == -1)
 	{
 		close(fd);
-		return(-1);
+		return (-1);
 	}
 	bytes = write(STDOUT_FILENO, &buf[0], bytes);
 	if (bytes == -1)
 	{
 		close(fd);
-		return(-1);
+		return (-1);
 	}
 	close(fd);
 	return (bytes);
