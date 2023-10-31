c16b9720c51742e30ffceb17c200b3843f9cf4e0
mavie <mave@yahoo.com>
2023-10-30 19:57:52 -0700

    m

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,43 +1,52 @@
-#include"main.h"
+#include "main.h"
 
 #define USAGE "Usage: cp file_from file_to\n"
-#define ERR_NOREAD "Error: Can't read from file NAME_OF_THE_FILE %s\n"
-#define ERR_NOWRITE "Error: Can't write to NAME_OF_THE_FILE %s\n"
-#define ERR_NOCLOSE "Can't close fd FD_VALUE %s\n"
+#define ERR_NOREAD "Error: Can't read from file %s\n"
+#define ERR_NOWRITE "Error: Can't write to file %s\n"
+#define ERR_NOCLOSE "Can't close fd %d\n"
 #define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
+
 /**
- *main - program
- *@argc: argument count
- *@argv: argument vector
+ * main - program entry point
+ * @argc: argument count
+ * @argv: argument vector
  *
- * return:1 on success abd exit on failure
+ * Return: 1 on success, and exit on failure
  */
 int main(int argc, char **argv)
 {
-	int from_fd = 0, to_fd = 0;
-	ssize_t b;
-	char buf[READ_BUF_SIZE];
+    int from_fd = 0, to_fd = 0;
+    ssize_t b;
+    char buf[READ_BUF_SIZE];
 
-	if (argc != 3)
-		dprintf(STDERR_FILENO, USAGE), exit(97);
-	from_fd = open(argv[1], O_RDONLY);
-	if (from_fd == -1)
-		dprintf(STDERR_FILENO, ERR_NOREAD, argv[1]), exit(98);
-	to_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
-	if (to_fd == -1)
-		dprintf(STDERR_FILENO, ERR_NOWRITE, argv[2], exit(99);
-				
-	while ((b = read(from_fd, buf, READ_BUF_SIZE)) > 0)
-		if (write(to_fd, buf, b) != b)
-		dprintf(STDERR_FILENO, ERR_NOWRITE, argv[2]), exit(99);
-	if (b == -1)
-	        dprintf(STDERR_FILENO, ERR_NOREAD, argv[1], exit(98);
-	
-	from_fd = close(from_fd);
-        to_fd = close(to_fd);
-	if (from_fd)
-		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
-	if (to_fd)
-		dprintf(STDERR_FILENO, ERR_NOCLOSE, to_fd), exit(100);
-	
-	return (EXIT_SUCCESS);	
+    if (argc != 3)
+        dprintf(STDERR_FILENO, USAGE), exit(97);
+
+    from_fd = open(argv[1], O_RDONLY);
+    if (from_fd == -1)
+        dprintf(STDERR_FILENO, ERR_NOREAD, argv[1]), exit(98);
+
+    to_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
+    if (to_fd == -1)
+        dprintf(STDERR_FILENO, ERR_NOWRITE, argv[2]), exit(99);
+
+    while ((b = read(from_fd, buf, READ_BUF_SIZE)) > 0)
+    {
+        if (write(to_fd, buf, b) != b)
+        {
+            dprintf(STDERR_FILENO, ERR_NOWRITE, argv[2]);
+            exit(99);
+        }
+    }
+
+    if (b == -1)
+        dprintf(STDERR_FILENO, ERR_NOREAD, argv[1]), exit(98);
+
+    if (close(from_fd) == -1)
+        dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
+
+    if (close(to_fd) == -1)
+        dprintf(STDERR_FILENO, ERR_NOCLOSE, to_fd), exit(100);
+
+    return (EXIT_SUCCESS);
+}	
