3ff7e0f9ead3724ebb1eb1b83f4017eb5652149f
mavie <mave@yahoo.com>
2023-10-30 19:51:16 -0700

    m

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -25,9 +25,10 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, ERR_NOREAD, argv[1]), exit(98);
 	to_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
 	if (to_fd == -1)
-		dprintf(STDERR_FILENO, ERR_NOWRITE, av[2], exit(99);
-		while ((b = read(from_fd, buf , READ_BUF_SIZE)) > 0)
-		if(write(to_fd, buf, b) != b)
+		dprintf(STDERR_FILENO, ERR_NOWRITE, argv[2], exit(99);
+				
+	while ((b = read(from_fd, buf, READ_BUF_SIZE)) > 0)
+		if (write(to_fd, buf, b) != b)
 		dprintf(STDERR_FILENO, ERR_NOWRITE, argv[2]), exit(99);
 	if (b == -1)
 	        dprintf(STDERR_FILENO, ERR_NOREAD, argv[1], exit(98);
