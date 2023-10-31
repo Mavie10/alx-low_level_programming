b0c087c3d193d2f3fcdd5524c4ccd6fb4b116658
mavie <mave@yahoo.com>
2023-10-30 18:41:42 -0700

    m

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,10 +14,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char buf[READ_BUF_SIZE * 8];
 
 	if (!filename || !letters)
-		return (-1);
+		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return (-1);
+		return (0);
 	bytes = read(fd, &buf[0], letters);
 	bytes = write(STDOUT_FILENO, &buf[0], bytes);
 	close(fd);
