8517ea9243f6fa9980af2321d7da35bb439883a8
mavie <mave@yahoo.com>
2023-10-30 17:31:24 -0700

    m

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,7 +2,7 @@
 /**
  * int create_file - erminated string to write to the file
  *
- * @filname: is the name of the file to create 
+ * @filname:is the name of the file to create
  * @text_content: content writed in the file
  *
  * return:1 on success, -1 on failure
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	int nletters;
 	int rwr;
 
-	if(!filename)
+	if (!filename)
 		return (-1);
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 06000);
 
@@ -21,7 +21,8 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (!text_content)
 		text_content = "";
-	for (nletters = 0; text_content[nletters]; nletters++);
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
 
 	rwr = write(fd, text_content, nletters);
 	if (rwr == -1)
